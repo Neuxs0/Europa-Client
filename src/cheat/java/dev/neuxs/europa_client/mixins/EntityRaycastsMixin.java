e0f82bab6d6ce1ce4b1d5b2268c54b420c4209e4
Lumyxen <lumyxen@pm.me>
2026-05-21 19:00:20 -0700

    Add reach distance to entity raycasts
    - Set maximum raycast distance from the Reach module
    - Register the new entity raycast mixin

---
+++ src/cheat/java/dev/neuxs/europa_client/mixins/EntityRaycastsMixin.java
@@ -0,0 +1,27 @@
+package dev.neuxs.europa_client.mixins;
+
+import com.badlogic.gdx.math.collision.Ray;
+import dev.neuxs.europa_client.modules.Modules;
+import finalforeach.cosmicreach.EntityRaycasts;
+import finalforeach.cosmicreach.entities.player.Player;
+import finalforeach.cosmicreach.world.Zone;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@SuppressWarnings({"unused", "FieldCanBeLocal"})
+@Mixin(EntityRaycasts.class)
+public abstract class EntityRaycastsMixin {
+    @Shadow
+    private float maximumRaycastDist;
+
+    @Inject(method = "raycastForEntities", at = @At("HEAD"))
+    private void europa_client$updateMaximumRaycastDistance(Zone zone, Ray ray, Player player,
+                                                            CallbackInfoReturnable<Boolean> cir) {
+        if (Modules.reach != null && Modules.reach.isEnabled()) {
+            this.maximumRaycastDist = Modules.reach.getReachDistance();
+        }
+    }
+}
