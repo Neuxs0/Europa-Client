44c317c28d331682153f3f17606e6226d62504f2
Lumyxen <lumyxen@pm.me>
2026-05-21 20:31:44 -0700

    Hide vanilla HUD modules from UI list
    - Filter out the vanilla healthbar and hotbar from UI page entries

--- src/cheat/java/dev/neuxs/europa_client/ui/pages/UIPage.java
+++ src/cheat/java/dev/neuxs/europa_client/ui/pages/UIPage.java
@@ -1,11 +1,22 @@
 package dev.neuxs.europa_client.ui.pages;
 
+import dev.neuxs.europa_client.modules.Module;
 import dev.neuxs.europa_client.modules.Modules;
 import dev.neuxs.europa_client.utils.rendering.BoxRenderer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SuppressWarnings("unused")
 public class UIPage extends ModuleListPage {
     public UIPage(BoxRenderer pageContainer) {
-        super("UI", pageContainer, () -> Modules.uiModuleList);
+        super("UI", pageContainer, UIPage::getListedUiModules);
+    }
+
+    private static List<Module> getListedUiModules() {
+        List<Module> modules = new ArrayList<>(Modules.uiModuleList);
+        modules.remove(Modules.vanillaHealthbar);
+        modules.remove(Modules.vanillaHotbar);
+        return modules;
     }
 }
